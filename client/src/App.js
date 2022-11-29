6b75101a6167c6f1eb601db99397c02a590cd6f3
Abdaleh Hersi <therealhersi03@gmail.com>
2022-11-29 22:26:41 +0000

    added home component boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import logo2 from "./logo.png";
 import './App.css';
+import Home from "./components/Home";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo2} className="App-logo" alt="logo" />
       </header>
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <h1>What ingredients do you want to use?</h1>
+    )
+}
+
+export default Home;
