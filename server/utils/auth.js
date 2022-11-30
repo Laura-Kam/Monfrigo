8e6667a41df1f819849f0f9fe872b2c8dd64ed4b
Inna 1201 <inna.ogurcova@gmail.com>
2022-11-30 17:26:46 +0000

    server files structure created

---
+++ server/utils/auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'secretrecipes';
+const expiration = '2h';
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+
+  // need to adjust according monfrigo sign up
+  signToken: function ({ firstName, email, _id }) {
+    const payload = { firstName, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
