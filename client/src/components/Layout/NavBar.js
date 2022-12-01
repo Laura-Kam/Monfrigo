9f4964832e527b4faffb155392a98b686b2b15cc
Laura Kambanis <kambanislaura@gmail.com>
2022-12-01 12:07:34 +0000

    changes to header and nav and background

--- client/src/components/Layout/NavBar.js
+++ client/src/components/Layout/NavBar.js
@@ -9,15 +9,23 @@ export default function NavBar() {
     >
       <div className="container-fluid">
         <nav aria-label="breadcrumb">
-          <ol className="breadcrumb">
+          <ol className="breadcrumb" style={{ fontFamily: "cormorant SC" }}>
             <li className="breadcrumb-item">
-              <a href="#">Home</a>
+              <a href="#Home">Home</a>
             </li>
             <li className="breadcrumb-item">
-              <a href="#">Library</a>
+              <a href="#library">Your recipes</a>
             </li>
             <li className="breadcrumb-item active" aria-current="page">
-              <a href="#">Data</a>
+              <a href="#data">About us</a>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              <a href="#data">Contact us</a>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              <a href="#data">
+                <span id="logout">Logout</span>
+              </a>
             </li>
           </ol>
         </nav>
