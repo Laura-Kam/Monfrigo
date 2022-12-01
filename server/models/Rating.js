dc4497db3fa84ff774911a722939e23cfd1d9b10
Inna 1201 <inna.ogurcova@gmail.com>
2022-12-01 19:05:49 +0000

    models created

---
+++ server/models/Rating.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+
+const { Schema } = mongoose;
+
+const ratingSchema = new Schema({
+  rating:{
+    type: Number,
+    required: true,
+  },
+  userId:{
+    id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+  },
+  recipeId: {
+    id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Recipe'
+    }
+  },
+  
+},
+{ timestamps: true });
+
+const Rating = mongoose.model('Rating', ingredientSchema);
+
+module.exports = Rating;
---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Product = require('./Recipe');
+const Category = require('./Rating');
+
+module.exports = { User, Recipe, Rating };
