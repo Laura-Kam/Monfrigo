c450e6b4fb8b33202844563f2ec71ae0f176a516
jsmall4 <64646369+jsmall4@users.noreply.github.com>
2022-12-05 17:09:01 +0000

    added to resolvers & mutations for recipes, login, fixed isPasswordCorrect bug, imported Recipe & Rating to index, added Auth to typeDefs, added lastName to auth.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,39 +2,49 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
-     _id: ID
-     firstName: String
-     lastName: String
-     email: String
-     recipes: [Recipe]
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    recipes: [Recipe]
   }
 
+  type Auth {
+    user: User
+    token: ID
+  }
 
   type Recipe {
-     _id: ID
-     name: String
-     cookingInstruction: String
-     ingredients: [String]
-     rating: [Rating]
+    _id: ID
+    name: String
+    cookingInstruction: String
+    ingredients: [String]
+    rating: [Rating]
   }
 
   type Rating {
-     rating: Int
-     userId: ID 
-     recipeId: ID 
+    rating: Int
+    userId: ID
+    recipeId: ID
   }
-  
-  type query {
-     user: User
-     recipes: [Recipe]
-     recipe(_id: ID): Recipe
-     
- }
+
+  type Query {
+    user: User
+    recipes: [Recipe]
+    recipe(_id: ID): Recipe
+  }
 
   type Mutation {
-     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-     login(email: String!, password: String!): Auth
-     favRecipe: Recipe
-     addRating(rating: Int, recipeId:ID): Recipe
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): Auth
+    login(email: String!, password: String!): Auth
+    favRecipe: Recipe
+    addRating(rating: Int, recipeId: ID): Recipe
   }
 `;
+
+module.exports = typeDefs;
