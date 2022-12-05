70c226bd79e185483216b10367baeb6625ba5df0
Laura Kambanis <kambanislaura@gmail.com>
2022-12-05 20:18:50 +0000

    added images

--- client/src/components/Home/Carousel.js
+++ client/src/components/Home/Carousel.js
@@ -74,6 +74,44 @@ export default function Carousel() {
           Our Recipes
         </p>
       </MDBCarouselItem>
+      <MDBCarouselItem
+        className="w-100 d-block square bg-primary rounded-5"
+        itemId={4}
+        src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+        alt="..."
+        style={{
+          height: "350px",
+          width: "450px",
+          rounded: "10px",
+        }}
+      >
+        <p
+          style={{
+            color: "#CF4A72",
+          }}
+        >
+          Our Recipes
+        </p>
+      </MDBCarouselItem>
+      <MDBCarouselItem
+        className="w-100 d-block square bg-primary rounded-5"
+        itemId={5}
+        src="https://images.unsplash.com/photo-1604908176997-125f25cc6f3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG1lYXQlMjBkaXNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        alt="..."
+        style={{
+          height: "350px",
+          width: "450px",
+          rounded: "10px",
+        }}
+      >
+        <p
+          style={{
+            color: "#CF4A72",
+          }}
+        >
+          Our Recipes
+        </p>
+      </MDBCarouselItem>
     </MDBCarousel>
   );
 }
