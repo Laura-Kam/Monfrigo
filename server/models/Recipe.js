b8078acadd67c23b6c97459bc2f8f7213c29e229
Laura Kambanis <kambanislaura@gmail.com>
2022-12-07 19:00:05 +0000

    changes

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -2,31 +2,14 @@ const { introspectionFromSchema } = require("graphql");
 const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
-// const Rating = require('./Rating');
+const Rating = require("./Rating");
 
-/* const ingredientSchema = new Schema({
-  product:{
+const ingredientSchema = new Schema({
+  product: {
     type: String,
     required: true,
   },
-}); */
-
-// const recipeSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   cookingInstruction: {
-//     type: String,
-//     // required: true,
-//   },
-//   ingredients: [String],
-//   rating: {
-//     type: Schema.Types.ObjectId,
-//     ref: 'Rating'
-//   },
-// },
-// { timestamps: true });
+});
 
 const recipeSchema = new Schema(
   {
@@ -34,18 +17,37 @@ const recipeSchema = new Schema(
       type: String,
       required: true,
     },
-    instructions: [String],
+    cookingInstruction: {
+      type: String,
+      required: true,
+    },
     ingredients: [String],
     rating: {
-      type: Number,
+      type: Schema.Types.ObjectId,
       ref: "Rating",
-    },
-    image: {
-      type: String,
     },
   },
   { timestamps: true }
 );
+
+// const recipeSchema = new Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     instructions: [String],
+//     ingredients: [String],
+//     rating: {
+//       type: Number,
+//       ref: "Rating",
+//     },
+//     image: {
+//       type: String,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
