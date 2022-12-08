5638c015df0401d52eec0eab8d34b34737acae3c
Laura Kambanis <kambanislaura@gmail.com>
2022-12-08 16:47:32 +0000

    button

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -30,25 +30,6 @@ const recipeSchema = new Schema(
   { timestamps: true }
 );
 
-// const recipeSchema = new Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//     },
-//     instructions: [String],
-//     ingredients: [String],
-//     rating: {
-//       type: Number,
-//       ref: "Rating",
-//     },
-//     image: {
-//       type: String,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
