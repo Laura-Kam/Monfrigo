dc82f8ad5b124a607ae381a329085b0c3147a314
Abdaleh Hersi <therealhersi03@gmail.com>
2022-12-08 19:18:53 +0000

    added queries and mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,38 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-      }
+mutation Login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    user {
+      email
     }
+    token
   }
+}
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($name: String!, $email: String!, $password: String!) {
-    addUser(name: $name, email: $email, password: $password) {
-      token
-      user {
-        _id
-      }
+mutation AddUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    user {
+      email
+      username
+    }
+    token
+  }
+}
+`;
+
+export const FAV_RECIPE = gql`
+mutation FavRecipe($recipe: RecipeInput!) {
+  favRecipe(recipe: $recipe) {
+    _id
+    email
+    recipes {
+      name
+      cookingInstruction
+      ingredients
     }
   }
+}
 `;
