530a50e604f45e9837946a048cf2d849dcad9106
Abdaleh Hersi <therealhersi03@gmail.com>
2022-12-08 20:27:55 +0000

    fixed duplicate saved recipes

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,15 +1,7 @@
 const { introspectionFromSchema } = require("graphql");
 const mongoose = require("mongoose");
-
 const { Schema } = mongoose;
 const Rating = require("./Rating");
-
-const ingredientSchema = new Schema({
-  product: {
-    type: String,
-    required: true,
-  },
-});
 
 const recipeSchema = new Schema(
   {
