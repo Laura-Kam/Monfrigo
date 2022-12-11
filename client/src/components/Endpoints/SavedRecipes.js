c68fdf3117b535634d7532ec7d0b43016d075b8f
Laura Kambanis <kambanislaura@gmail.com>
2022-12-11 11:28:22 +0000

    font

--- client/src/components/Endpoints/SavedRecipes.js
+++ client/src/components/Endpoints/SavedRecipes.js
@@ -2,41 +2,51 @@ import React from "react";
 import { GET_ME } from "../../utils/queries";
 import Card from "../Home/Card";
 import { useQuery } from "@apollo/client";
-import LinearProgress from '@mui/material/LinearProgress';
+import LinearProgress from "@mui/material/LinearProgress";
 
 export default function SavedRecipes() {
-    const { loading, error, data } = useQuery(GET_ME);
-    const userData = data?.user.recipes || [];
-    const rasberryFlapjack = () => {
-        return loading ? <LinearProgress /> : null
-    }
+  const { loading, error, data } = useQuery(GET_ME);
+  const userData = data?.user.recipes || [];
+  const rasberryFlapjack = () => {
+    return loading ? <LinearProgress /> : null;
+  };
 
-    return (
-        <div>
-            <div
-                className="card text-center"
-                style={{
-                    border: "5px",
-                    backgroundColor: "white",
-                    borderColor: "#FFE7E9",
-                    fontFamily: "cormorant CS",
-                }}
-            >
-                <div className="card-body">
-                    <h1 className="card-title" style={{ fontFamily: "cormorant CS" }}>Enjoy your favourite recipes</h1>
-                    <span className="material-symbols-outlined" style={{
-                        display: "block",
-                        margin: "0.4rem",
-                        color: "#e75480",
-                    }}>
-                        favorite
-                    </span>
-                </div>
-            </div>
-            {rasberryFlapjack()}
-            <div className="cardContainer">
-                {userData.map((recipe) => (<Card data={recipe} saved={true} />))}
-            </div>
+  return (
+    <div>
+      <div
+        className="card text-center"
+        style={{
+          border: "5px",
+          backgroundColor: "white",
+          borderColor: "#FFE7E9",
+          fontFamily: "cormorant CS",
+        }}
+      >
+        <div className="card-body">
+          <h1
+            className="card-title"
+            style={{ fontFamily: "cormorant CS", fontSize: "29px" }}
+          >
+            Enjoy your favourite recipes
+          </h1>
+          <span
+            className="material-symbols-outlined"
+            style={{
+              display: "block",
+              margin: "0.4rem",
+              color: "#e75480",
+            }}
+          >
+            favorite
+          </span>
         </div>
-    )
+      </div>
+      {rasberryFlapjack()}
+      <div className="cardContainer">
+        {userData.map((recipe) => (
+          <Card data={recipe} saved={true} />
+        ))}
+      </div>
+    </div>
+  );
 }
