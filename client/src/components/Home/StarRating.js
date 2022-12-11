61cdb46daa26369a8ac739a469e59cf78718a812
Laura Kambanis <kambanislaura@gmail.com>
2022-12-11 11:52:25 +0000

    style changes

--- client/src/components/Home/StarRating.js
+++ client/src/components/Home/StarRating.js
@@ -1,33 +1,35 @@
-import React, { useState } from 'react';
-import { FaStar} from "react-icons/fa";
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
 
 const StarRating = () => {
-const [rating, setRating] = useState(null);
-const [hover, setHover] = useState(null);
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
 
-    return <div>{[...Array(5)].map((star, i) => {
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
         const ratingValue = i + 1;
 
         return (
-            <label>
-                <input type="radio" 
-                name="rating" 
-                value={ratingValue} 
-                onClick={()=> setRating(ratingValue)}
-        
-                />
-                <FaStar className="star" 
-                color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"} 
-                size={25} 
-                onMouseEnter={() => setHover(ratingValue)}
-                onMouseLeave={() => setHover(null)}
-                />
-            </label>
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <FaStar
+              className="star"
+              color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+              size={25}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+            />
+          </label>
         );
-    })}
-    
+      })}
     </div>
-    
-}
+  );
+};
 
-export default StarRating
+export default StarRating;
